1fcba45bb4ea92d521cc764009a4e4e02a8decd1
Romaric HONOREZ <romaric.honorez@student.vinci.be>
2019-09-24 12:11:29 +0200

    fussion dans le nouveau dossier

--- modules/server.js
+++ modules/server.js
@@ -4,6 +4,7 @@
 
 const express = require('express');
 const usersRouter = require('../routes/users');
+const quotesRouter = require('../routes/quotes');
 
 
 /**
@@ -23,6 +24,7 @@ const app = express();
 
 // Configure routes
 app.use('/users', usersRouter);
+app.use('/quotes', quotesRouter);
 
 // Start server
 var start = function (callback) {
