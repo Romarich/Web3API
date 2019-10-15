b90976db2baa0a374ffc2f24a1549f56867905ac
Romaric HONOREZ <romaric.honorez@student.vinci.be>
2019-10-15 11:05:44 +0200

    rajout des anciens trucs

--- routes/quotes.js
+++ routes/quotes.js
@@ -4,6 +4,7 @@
 
 var express = require('express')
 var router = express.Router()
+const db = require('../modules/db')
 
 
 
@@ -11,13 +12,34 @@ var router = express.Router()
  * Routes
  */
 
-// Return a quote
-router.get('/', function (req, res) {
-    res.json({
-         message: 'I\'ve never met a strong person with an easy past',
-         author : 'Atticus'
-    });
+// Return all quotes
+router.get('/all', function (req, res) {
+    db.db.collection('Quotes').find({}).toArray(function(err, result) {
+        res.json(result)
+    })
 })
+// Return a simple quote
+router.get('/:id', function (req, res) {
+    var idPourLaPage = req.params["id"];
+    db.db.collection('Quotes').find({realId : idPourLaPage}).toArray(function(err, result) {
+        res.json(result)
+    })
+})
+
+// Delete a simple quote
+router.get('/delete/:id', function (req, res) {
+    var idPourLaPage = req.params["id"];
+    db.db.collection('Quotes').deleteOne({realId : idPourLaPage})
+    return res.json(idPourLaPage+" a bien été delete")
+})
+
+// Update a simple quote
+router.get('/update/:id', function (req, res) {
+    var idPourLaPage = req.params["id"];
+    db.db.collection('Quotes').updateOne({realId : idPourLaPage},{$set: {realId : "6"}})
+    return res.json(idPourLaPage+" a bien été update")
+})
+
 
 
 /**
